e585a9870e2e2415133ad0107fe27442c4f12ed9
rhaiz <rhaissazeferino@gmail.com>
2025-08-12 22:40:01 -0300

    Feat: final cart and buy screen

--- src/app/product-variant/[slug]/page.tsx
+++ src/app/product-variant/[slug]/page.tsx
@@ -2,7 +2,6 @@ import { eq } from "drizzle-orm";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-import Footer from "@/components/common/footer";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
 import { productTable, productVariantTable } from "@/db/schema";
@@ -37,50 +36,42 @@ const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
     },
   });
   return (
-    <>
-      <div className="flex flex-col space-y-6">
-        <Image
-          src={productVariant.imageUrl}
-          alt={productVariant.name}
-          sizes="100vw"
-          height={0}
-          width={0}
-          className="h-auto w-full object-cover"
+    <div className="flex flex-col space-y-6">
+      <Image
+        src={productVariant.imageUrl}
+        alt={productVariant.name}
+        sizes="100vw"
+        height={0}
+        width={0}
+        className="h-auto w-full object-cover"
+      />
+
+      <div className="px-5">
+        <VariantSelector
+          selectedVariantSlug={productVariant.slug}
+          variants={productVariant.product.variants}
         />
-
-        <div className="px-5">
-          <VariantSelector
-            selectedVariantSlug={productVariant.slug}
-            variants={productVariant.product.variants}
-          />
-        </div>
-
-        <div className="px-5">
-          {/* DESCRIÇÃO */}
-          <h2 className="text-lg font-semibold">
-            {productVariant.product.name}
-          </h2>
-          <h3 className="text-muted-foreground text-sm">
-            {productVariant.name}
-          </h3>
-          <h3 className="text-lg font-semibold">
-            {formatCentsToBRL(productVariant.priceInCents)}
-          </h3>
-        </div>
-
-        <ProductActions productVariantId={productVariant.id} />
-
-        <div className="px-5">
-          <p className="text-shadow-amber-600">
-            {productVariant.product.description}
-          </p>
-        </div>
-
-        <ProductList title="Talvez você goste" products={likelyProducts} />
-
-        <Footer />
       </div>
-    </>
+
+      <div className="px-5">
+        {/* DESCRIÇÃO */}
+        <h2 className="text-lg font-semibold">{productVariant.product.name}</h2>
+        <h3 className="text-muted-foreground text-sm">{productVariant.name}</h3>
+        <h3 className="text-lg font-semibold">
+          {formatCentsToBRL(productVariant.priceInCents)}
+        </h3>
+      </div>
+
+      <ProductActions productVariantId={productVariant.id} />
+
+      <div className="px-5">
+        <p className="text-shadow-amber-600">
+          {productVariant.product.description}
+        </p>
+      </div>
+
+      <ProductList title="Talvez você goste" products={likelyProducts} />
+    </div>
   );
 };
 
